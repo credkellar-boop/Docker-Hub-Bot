70878fce859eea87be5460cd39e36665ff40e3ed
Darion Kellar <credkellar@gmail.com>
2026-06-19 00:28:13 -0700

    Add BackupManager class for cloud synchronization

---
+++ utils/backup_manager.py
@@ -0,0 +1,4 @@
+class BackupManager:
+    def sync_to_cloud(self):
+        """Syncs encrypted data vault to remote secure storage."""
+        print("[*] Backup Agent: Encrypting and uploading audit trail...")
