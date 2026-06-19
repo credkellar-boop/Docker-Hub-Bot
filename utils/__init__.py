4e9448de29667d7c1a1fa852f7b49022e7ee70b5
Darion Kellar <credkellar@gmail.com>
2026-06-19 01:36:55 -0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,8 @@
- 
+     def __init__(self):
+        self.library_repos = [
+            "docker-library/official-images",
+            "docker-library/docs",
+            "docker-library/repo-info",
+            "docker-library/faq" 
+        ]
+        self.workspace = "data/repos"
